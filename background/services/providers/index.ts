aa46bdf9572ab5b3bdbe38eab72b860735904b20
Artem Burakov <artem@burakov.com>
2024-06-22 13:51:37 +0300

    factory provider proposal

--- background/services/providers/index.ts
+++
@@ -1,9 +0,0 @@
-import { JsonRpcProvider } from "quais"
-
-export default class PelagusJsonRpcProvider extends JsonRpcProvider {
-  rpcUrls: string | string[]
-
-  constructor(rpcUrls: string[] | string) {
-    super(rpcUrls)
-  }
-}
