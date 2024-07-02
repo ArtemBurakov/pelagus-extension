3feceb1633deb5108d7f12ca110d90a5aa43b2ac
Artem Burakov <artem@burakov.com>
2024-07-02 11:14:37 +0300

    changes function to fetch address balance & minor changes

--- background/constants/networks/networks.ts
+++ background/constants/networks/networks.ts
@@ -4,7 +4,7 @@ export const QuaiNetworkGA: NetworkInterfaceGA = {
   chainID: "9000",
   baseAsset: { name: "Quai Network", symbol: "QUAI", decimals: 18 },
   family: "EVM",
-  rpcUrls: ["http://rpc.sandbox.quai.network/"],
+  rpcUrls: ["http://rpc.sandbox.quai.network"],
   blockExplorerURL: "",
   chains: [
     {
