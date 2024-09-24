1ce54216b3304c6cd1bdf308e70d45c4c598cc72
Artem Burakov <artem@burakov.com>
2024-09-24 15:30:42 +0300

    rewrites dapp switch & contracts deployments

--- background/services/transactions/index.ts
+++ background/services/transactions/index.ts
@@ -2,7 +2,12 @@ import {
   QuaiTransactionRequest,
   QuaiTransactionResponse,
 } from "quais/lib/commonjs/providers"
-import { getZoneForAddress, QuaiTransaction, TransactionReceipt } from "quais"
+import {
+  getZoneForAddress,
+  QuaiTransaction,
+  TransactionReceipt,
+  Zone,
+} from "quais"
 
 import BaseService from "../base"
 import ChainService from "../chain"
@@ -117,13 +122,10 @@ export default class TransactionService extends BaseService<TransactionServiceEv
   ): Promise<void> {
     try {
       const { jsonRpcProvider } = this.chainService
-      const { to, serialized: signedTransaction } = quaiTransaction
+      const { to, from, serialized: signedTransaction } = quaiTransaction
 
-      if (!to) {
-        throw new Error("Transaction 'to' field is not specified.")
-      }
-
-      const zone = getZoneForAddress(to)
+      const zoneTargetAddress = to || from || Zone.Cyprus1
+      const zone = getZoneForAddress(zoneTargetAddress)
       if (!zone) {
         throw new Error(
           "Invalid address shard: Unable to determine the zone for the given 'to' address."
@@ -285,8 +287,7 @@ export default class TransactionService extends BaseService<TransactionServiceEv
     const transaction = await this.db.getQuaiTransactionByHash(receipt.hash)
     if (!transaction) return
 
-    const { status, blockHash, blockNumber, gasPrice, gasUsed, etxs, logs } =
-      receipt
+    const { status, blockHash, blockNumber, gasPrice, gasUsed } = receipt
 
     if (status === 1) {
       transaction.status = QuaiTransactionStatus.CONFIRMED
@@ -303,8 +304,8 @@ export default class TransactionService extends BaseService<TransactionServiceEv
     transaction.blockNumber = blockNumber
     transaction.gasPrice = gasPrice
     transaction.gasUsed = gasUsed
-    transaction.etxs = [...etxs]
-    transaction.logs = [...logs]
+    transaction.etxs = [] // TODO
+    transaction.logs = [] // TODO
 
     await this.saveQuaiTransaction(transaction)
   }
